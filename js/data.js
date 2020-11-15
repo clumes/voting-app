be043e4bf3566f147851cb7b0f4912d92a95a26f
Rafael Molla <rafaelmolladouglas@gmail.com>
2020-11-15 20:19:57 -0300

    Cards are now functional

---
+++ js/data.js
@@ -0,0 +1,14 @@
+const candidates = [
+    {
+        id: "trump",
+        name: "Donald Trump",
+        party: "Republican Party",
+        votes: 260,
+    },
+    {
+        id: "biden",
+        name: "Joe Biden",
+        party: "Democratic Party",
+        votes: 260,
+    }
+]
